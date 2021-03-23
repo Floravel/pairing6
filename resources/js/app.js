26c1a4831b671b79c5eb8e58e45743d0f4d75d22
floravel <flo-hess1@web.de>
2021-03-23 06:27:24 +0100

    set up vue.js, include tailwindcss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
+import Vue from 'vue';
+import router from "./router";
+import App from "./components/App";
+
 require('./bootstrap');
+
+/*window.Vue = require('vue').default;*/
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        App
+    },
+    router
+});
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Componento!</div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+}
+</script>
+
+<style scoped>
+
+</style>
