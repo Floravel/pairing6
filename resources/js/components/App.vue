f19b9bc7cf28c3022b3e10caebfc6bb6ec492c2e
floravel <flo-hess1@web.de>
2021-04-29 12:25:49 +0200

    add sticky footer

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,10 +2,10 @@
     <div class="bg-blue-500 flex flex-1 flex-col h-screen overflow-y-hidden">
         <Nav class="fixed"/>
 
-        <div class="flex overflow-y-hidden flex-1">
-            <Sidebar/>
+        <div class="mt-12 flex overflow-y-hidden flex-1">
+            <Sidebar class="sm:mt-18 md:mt-22 lg:mt-12 xl:mt-8 mt-20" />
 
-            <div class="overflow-x-hidden w-2/3">
+            <div class="scroll-bar-hidden overflow-x-hidden w-2/3">
                 <router-view></router-view>
             </div>
         </div>
@@ -30,5 +30,18 @@ export default {
 </script>
 
 <style scoped>
+    .scroll-bar-hidden {
+        overflow-y: scroll; /* Add the ability to scroll */
+    }
 
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    .scroll-bar-hidden::-webkit-scrollbar {
+        display: none;
+    }
+
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .scroll-bar-hidden {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
 </style>
